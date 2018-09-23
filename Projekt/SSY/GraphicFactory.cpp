51b16a465b38bf9c08b5df12e025ac3a50e2ce27
ErnietheChicken <darkrider852@gmx.de>
2018-09-23 13:43:29 +0200

    Laden der Bilder sowie Texturen optimiert

--- Projekt/SSY/GraphicFactory.cpp
+++ Projekt/SSY/GraphicFactory.cpp
@@ -2,6 +2,8 @@
 
 
 bool GraphicFactory::isImageInit = false;
+std::map<std::string, SDL_Texture*> GraphicFactory::textures;
+std::map<std::string, SDL_Surface*> GraphicFactory::surfaces;
 
 // Initialisiert Bibliothek wenn sie noch nicht initialisiert wurde
 void GraphicFactory::initIMG()
@@ -20,29 +22,86 @@ void GraphicFactory::initIMG()
 //Lädt ein Bild
 SDL_Surface * GraphicFactory::loadPicture(const char * path)
 {
-	std::string completePath = "../Bilder/" + std::string(path);
-	SDL_Surface* picture = nullptr;
+	SDL_Surface* picture = NULL;
+	std::string key = std::string(path);
+	if (surfaces.find(std::string(key)) == surfaces.end())
+	{
+		std::string completePath = "../Bilder/" + std::string(path);
 
-	picture = IMG_Load(completePath.c_str());
-	if (picture == NULL)
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, IMG_GetError());
+		picture = IMG_Load(completePath.c_str());
+		if (picture == NULL)
+			SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, IMG_GetError());
+	}
+	else
+	{
+		SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "Bild %s wurde schon geladen.", path);
+	}
 
 	return picture;
+}
+
+SDL_Surface * GraphicFactory::getPicture(const char * path)
+{
+	SDL_Surface * picture = NULL;
+
+	std::string key = std::string(path);
+	if (surfaces.find(std::string(key)) != surfaces.end())
+	{
+		picture = surfaces.at(std::string(key));
+	}
+	else
+	{
+		picture = loadPicture(path);
+	}
+
+	return picture;
+}
+
+SDL_Texture * GraphicFactory::getTexture(SDL_Renderer * renderer, const char * path)
+{
+	SDL_Texture * texture = NULL;
+
+	std::string key = std::string(path);
+	if (textures.find(std::string(key)) != textures.end())
+	{
+		texture = textures.at(std::string(key));
+	}
+	else
+	{
+		texture = loadTexture(renderer, path);
+	}
+
+	return texture;
 }
 
 // Lädt eine Texture
 SDL_Texture * GraphicFactory::loadTexture(SDL_Renderer * renderer, const char * path)
 {
+	SDL_Texture* texture = NULL;
+
 	if (renderer == nullptr)
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Renderer zum erstellen einer Textur darf nicht NULL sein.");
+	else
+	{
 
-	SDL_Texture* texture = nullptr;
-	SDL_Surface* picture = loadPicture(path);
+		std::string key = std::string(path);
+		if (textures.find(std::string(key)) == textures.end())
+		{
+			SDL_Surface* picture = loadPicture(path);
+			texture = SDL_CreateTextureFromSurface(renderer, picture);
+			SDL_FreeSurface(picture);	// Speicher wieder frei geben
+			picture = NULL;
 
-	texture = SDL_CreateTextureFromSurface(renderer, picture);
-	SDL_FreeSurface(picture);	// Speicher wieder frei geben
-	if (texture == nullptr)
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, SDL_GetError());
+			if (texture == nullptr)
+				SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, SDL_GetError());
+			else
+				textures.insert(std::pair<std::string, SDL_Texture*>(key, texture));
+		}
+		else
+		{
+			SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "Textur %s wurde schon geladen.", path);
+		}
+	}
 
 	return texture;
 }
@@ -63,5 +122,15 @@ SDL_Texture * GraphicFactory::loadTextureFromSurface(SDL_Renderer * renderer, SD
 
 void GraphicFactory::quit()
 {
+	for (std::map<std::string, SDL_Surface*>::iterator it = surfaces.begin(); it != surfaces.end(); ++it)
+	{
+		SDL_FreeSurface(it->second);
+		it->second = NULL;
+	}
+	for (std::map<std::string, SDL_Texture*>::iterator it = textures.begin(); it != textures.end(); ++it)
+	{
+		SDL_DestroyTexture(it->second);
+	}
+
 	IMG_Quit();
 }
--- Projekt/SSY/GraphicFactory.h
+++ Projekt/SSY/GraphicFactory.h
@@ -2,18 +2,62 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <string>
+#include <map>
 #include "GenericError.h"
 
 class GraphicFactory
 {
 private:
 	static bool isImageInit;
+	static std::map<std::string, SDL_Texture*> textures;
+	static std::map<std::string, SDL_Surface*> surfaces;
 
 public:
 	static void initIMG();
 
+	/**
+	Läd ein Bild aus dem Bilder Ordner.
+
+	@param path Pfad zum Bild.
+
+	@return Geladenes Bild oder NULL, wenn das Bild schon geladen wurde.
+	*/
 	static SDL_Surface* loadPicture(const char* path);
+
+	/**
+	Sucht eine geladenes Bild.
+
+	@param path Pfad zum Bild.
+
+	@return Das gesuchte Bild.
+	*/
+	static SDL_Surface* getPicture(const char* path);
+
+	/**
+	Sucht eine geladene Textur.
+
+	@param path Pfad zum Bild welches als Textur geladen werden soll.
+
+	@return geladene Textur.
+	*/
+	static SDL_Texture* getTexture(SDL_Renderer * renderer, const char* path);
+
+	/**
+	Läd eine Textur von dem übergebenen Bild.
+
+	@param path Pfad zum Bild, von welchem die Textur geladen werden soll.
+
+	@return geladene Textur oder NULL, wenn die Texture schon geladen wurde.
+	*/
 	static SDL_Texture* loadTexture(SDL_Renderer * renderer, const char* path);
+
+	/**
+	Läd eine Textur von dem übergebenen Bild.
+
+	@param path Pfad zum Bild, von welchem die Textur geladen werden soll.
+
+	@return geladene Textur oder NULL, wenn ein Fehler aufgetreten ist.
+	*/
 	static SDL_Texture* loadTextureFromSurface(SDL_Renderer * renderer, SDL_Surface* surface);
 
 	static void quit();
