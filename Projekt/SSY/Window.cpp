ec9ced758e3449ef4ffcd75ebb3edd50120163e5
ErnietheChicken <darkrider852@gmx.de>
2018-09-23 19:59:07 +0200

    Window durch Menü erweitert

--- Projekt/SSY/Window.cpp
+++ Projekt/SSY/Window.cpp
@@ -1,6 +1,5 @@
 #include "Window.h"
-
-
+#include "Core.h"
 
 Window::Window()
 {
@@ -28,14 +27,29 @@ bool Window::createWindow()
 		background = renderer->getTexture("Testscreen.bmp");
 	}
 
+	initWindow();
+
 	return ret;
 }
 
 void Window::onRender()
 {
 	renderer->drawBackground(background);
-	
+	if (activeMenu != NULL)
+		activeMenu->onPaint(renderer);
+
+	for (std::list<ObjectControl*>::iterator it = this->objects.begin();
+		it != this->objects.end(); ++it)
+	{
+		(*it)->onPaint(renderer);
+	}
+
 	renderer->presentRenderer();
+}
+
+void Window::initWindow()
+{
+	// TODO: Menü nur einmal zentral erstellen
 }
 
 SDL_Window* Window::getWindow()
@@ -45,10 +59,26 @@ SDL_Window* Window::getWindow()
 
 void Window::addObject(ObjectControl * object)
 {
-	if (object != NULL && (std::find(objects.begin(), objects.end(), object) != objects.end()))
+	if (object != NULL && !(std::find(objects.begin(), objects.end(), object) != objects.end()))
 	{
 		this->objects.push_back(object);
 	}
+}
+
+void Window::addObjects(ObjectControl *object ...)
+{
+	ObjectControl* control = object;
+
+	va_list objects;
+	va_start(objects, control);
+
+	while (control != NULL)
+	{
+		this->addObject(control);
+		control = va_arg(objects, ObjectControl*);
+	}
+
+	va_end(objects);
 }
 
 void Window::removeObject(ObjectControl * object)
@@ -59,8 +89,42 @@ void Window::removeObject(ObjectControl * object)
 	}
 }
 
+void Window::setMenu(Menu * menu)
+{
+	if (activeMenu != menu)
+	{
+		delete activeMenu; // Speicher wieder freigeben wenn es nicht das gleiche Menü ist
+
+		if (menu != NULL)
+		{
+			std::list<GUIElement*> list = menu->getObjects();
+			for (std::list<GUIElement*>::iterator it = list.begin();
+				it != list.end(); ++it)
+			{
+				this->addObject(*it);
+			}
+		}
+	}
+	this->activeMenu = menu;
+}
+
+std::list<ObjectControl*> Window::getObjects()
+{
+	return this->objects;
+}
+
+Menu * Window::getMenu()
+{
+	return this->activeMenu;
+}
+
 Window::~Window()
 {
+	if (activeMenu != NULL)
+	{
+		delete activeMenu;
+	}
+	objects.clear();
 	delete renderer;
 	SDL_DestroyTexture(background);
 	SDL_DestroyWindow(window);
--- Projekt/SSY/Window.h
+++ Projekt/SSY/Window.h
@@ -2,6 +2,8 @@
 #include <SDL.h>
 #include <list>
 #include <algorithm>
+#include "Menu.h"
+#include "Button.h"
 #include "ObjectControl.h"
 #include "GenericError.h"
 #include "Renderer.h"
@@ -10,19 +12,35 @@ class Window
 {
 private:
 	std::list<ObjectControl*> objects;
+	Menu* activeMenu;
 
 	SDL_Window * window = nullptr;
 	Renderer * renderer = nullptr;
 	SDL_Texture* background = nullptr;
 
+	void initWindow();
+
 public:
 	Window();
 	bool createWindow();
 	void onRender();
-	SDL_Window* getWindow();
 
 	void addObject(ObjectControl* object);
+
+	/**
+	Funktion fügt ObjectControls dem Fenster hinzu.
+	WICHTIG: Man muss als letztes Argument NULL übergeben da sonst eine Speicherzugriffsverletzung stattfindet.
+
+	@params object Liste von ObjectControls welche hinzugefügt werden sollen unnd am Ende NULL
+	*/
+	void addObjects(ObjectControl* object...);
 	void removeObject(ObjectControl* object);
+
+	void setMenu(Menu* menu);
+
+	std::list<ObjectControl*> getObjects();
+	Menu* getMenu();
+	SDL_Window* getWindow();
 
 	~Window();
 };
