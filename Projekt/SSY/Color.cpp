8c50a026d57769799610b5fd7dc85f0963ed3775
ErnietheChicken <darkrider852@gmx.de>
2018-09-27 12:14:56 +0200

    Color-Klasse hinzugefügt

---
+++ Projekt/SSY/Color.cpp
@@ -0,0 +1,24 @@
+#include "Color.h"
+
+// Basic Colors
+
+const SDL_Color Color::BLACK = SDL_Color{ 0, 0, 0, 255 };
+const SDL_Color Color::SILVER = SDL_Color{ 192, 192, 192, 255 };
+const SDL_Color Color::GRAY = SDL_Color{ 128, 128, 128, 255 };
+const SDL_Color Color::WHITE = SDL_Color{ 255, 255, 255, 255 };
+const SDL_Color Color::MAROON = SDL_Color{ 128, 0, 0, 255 };
+const SDL_Color Color::RED = SDL_Color{ 255, 0, 0, 255 };
+const SDL_Color Color::PURPLE = SDL_Color{ 128, 0, 128, 255 };
+const SDL_Color Color::FUCHSIA = SDL_Color{ 255, 0, 255, 255 };
+const SDL_Color Color::GREEN = SDL_Color{ 0, 128, 0, 255 };
+const SDL_Color Color::LIME = SDL_Color{ 0, 255, 0, 255 };
+const SDL_Color Color::OLIVE = SDL_Color{ 128, 128, 0, 255 };
+const SDL_Color Color::YELLOW = SDL_Color{ 255, 255, 0, 255 };
+const SDL_Color Color::NAVY = SDL_Color{ 0, 0, 128, 255 };
+const SDL_Color Color::BLUE = SDL_Color{ 0, 0, 255, 255 };
+const SDL_Color Color::TEAL = SDL_Color{ 0, 128, 128, 255 };
+const SDL_Color Color::AQUA = SDL_Color{ 0, 255, 255, 255 };
+
+// Other Colors
+
+const SDL_Color Color::TRANSPARENT = SDL_Color{ 0, 0, 0, 0 };
---
+++ Projekt/SSY/Color.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <SDL.h>
+
+class Color
+{
+public:
+	static SDL_Color fromRGB(Uint8 r, Uint8 g, Uint8 b)
+	{
+		return SDL_Color{ r, g, b, 255 };
+	}
+	static SDL_Color fromARGB(Uint8 r, Uint8 g, Uint8 b, Uint8 a)
+	{
+		return SDL_Color{ r, g, b, a };
+	}
+
+	// Basic Colors
+
+	static const SDL_Color BLACK;
+	static const SDL_Color SILVER;
+	static const SDL_Color GRAY;
+	static const SDL_Color WHITE;
+	static const SDL_Color MAROON;
+	static const SDL_Color RED;
+	static const SDL_Color PURPLE;
+	static const SDL_Color FUCHSIA;
+	static const SDL_Color GREEN;
+	static const SDL_Color LIME;
+	static const SDL_Color OLIVE;
+	static const SDL_Color YELLOW;
+	static const SDL_Color NAVY;
+	static const SDL_Color BLUE;
+	static const SDL_Color TEAL;
+	static const SDL_Color AQUA;
+
+	// Other Colors
+
+	static const SDL_Color TRANSPARENT;
+};
