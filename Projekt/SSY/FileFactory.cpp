558a0f100301bef7a40b92162f0a0017e2fb4aa5
ErnietheChicken <darkrider852@gmx.de>
2018-11-27 10:50:00 +0100

    FileFactory hinzugefügt; Lesen und Schreiben in Datei möglich

---
+++ Projekt/SSY/FileFactory.cpp
@@ -0,0 +1,81 @@
+#include "FileFactory.h"
+#include <fstream>
+#include <SDL.h>
+
+void FileFactory::writeLine(std::string file, std::string text, const char mode)
+{
+	std::ofstream destinationFile;
+	destinationFile.open(file, mode);
+
+	if (!destinationFile.is_open())
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Datei %s konnte nicht geöffnet werden", file);
+	}
+	else
+	{
+		destinationFile << text;
+		if (text.substr(text.size() - 1) != "\n")
+		{
+			destinationFile << "\n";
+		}
+
+		destinationFile.close();
+		if (destinationFile.is_open())
+		{
+			SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Datei %s konnte nicht geschlossen werden", file);
+		}
+	}
+
+}
+
+std::list<std::string> FileFactory::readLines(std::string file)
+{
+	std::list<std::string> linesList;
+	std::ifstream destinationFile;
+	destinationFile.open(file);
+	if (!destinationFile.is_open())
+	{
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Datei %s konnte nicht geöffnet werden", file);
+	}
+	else
+	{
+		while (!destinationFile.eof())
+		{
+			std::string line;
+			std::getline(destinationFile, line);
+			linesList.push_back(line);
+		}
+
+		destinationFile.close();
+		if (destinationFile.is_open())
+		{
+			SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Datei %s konnte nicht geschlossen werden", file);
+		}
+	}
+
+	return linesList;
+}
+
+std::map<std::string, std::string> FileFactory::read(std::string file)
+{
+	std::map<std::string, std::string> resultMap;
+	std::list<std::string> lines = readLines(file);
+
+	for (std::list<std::string>::iterator it = lines.begin(); it != lines.end(); ++it)
+	{
+		std::string line = (*it);
+		std::string key = line.substr(0, line.find('='));
+		// Trim String
+		key.erase(0, key.find_first_not_of(" "));
+		key.erase(key.find_last_not_of(" ") + 1);
+
+		std::string value = line.substr(line.find('=') + 1);
+		// Trim String
+		value.erase(0, value.find_first_not_of(" "));
+		value.erase(value.find_last_not_of(" ") + 1);
+
+		resultMap.insert(std::pair<std::string, std::string>(key, value));
+	}
+
+	return resultMap;
+}
---
+++ Projekt/SSY/FileFactory.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <string>
+#include <list>
+#include <map>
+#include <sstream>
+
+class FileFactory
+{
+public:
+
+	/**
+	Schreibt eine Zeile in die übergebene Datei.
+
+	@param file Pfad zur Datei.
+	@param text Text welcher in eine Zeile geschrieben werden soll.
+	*/
+	static void writeLine(std::string file, std::string text, const char mode = std::ios::app);
+
+	/**
+	Schreibt die übergebenen Value-Wert Paare in die übergebene Datei.
+
+	@param file Pfad zur Datei.
+	@param map Key-Value Paare welche in die Datei geschrieben werden soll.
+	*/
+	template <typename V, typename K>
+	static void write(std::string file, std::map<V, K> map);
+
+	/**
+	Liest die Datei Zeilenweise in ein String-Array.
+
+	@param file Pfad zur Datei.
+	@return String-List Einzelne Zeilen der Datei.
+	*/
+	static std::list<std::string> readLines(std::string file);
+
+	/**
+	Liest die Datei Zeilenweise in eine Map aus Key-Value Paaren.
+	Die Konvention muss 'Key = Value' entsprechen.
+
+	@param file Pfad zur Datei.
+	@return map Key-Value Paare der Datei.
+	*/
+	static std::map<std::string, std::string> read(std::string file);
+};
+
+template<typename K, typename V>
+inline void FileFactory::write(std::string file, std::map<K, V> map)
+{
+	for (auto it = map.begin(); it != map.end(); ++it)
+	{
+		std::ostringstream keyStream;
+		std::ostringstream valueStream;
+
+		keyStream << it->first;
+		std::string key = keyStream.str();
+
+		valueStream << it->second;
+		std::string value = valueStream.str();
+
+		writeLine(file, key + " = " + value + "\n");
+	}
+}
